80d61823c7ecd96991c97df12a7075b963744ae5
stephassoria <stephane.capo@assoria.com>
2019-11-06 16:37:25 +0100

    Little bug fix for building on Emscripten platform
    Former-commit-id: ac2072b678e2dbb6d88ec0f80bf3dd7db457d2c8
    Former-commit-id: 5aed2a9dfd3e06742dbcd3aed8bff1772d033b9c

--- framework/XML/Headers/XMLAttribute.h
+++ framework/XML/Headers/XMLAttribute.h
@@ -102,8 +102,10 @@ private:
 
 typedef XMLAttributeTemplate<std::string> XMLAttribute;
 
+#ifndef JAVASCRIPT
 template<typename T>
 IMPLEMENT_PREALLOCATED_NEW(XMLAttributeTemplate<T>, 4096)
+#endif
 
 #if (__cplusplus >= 201703L || _MSVC_LANG  >= 201703L) && !defined(JAVASCRIPT) && !defined(__clang__)
 #include <charconv>
