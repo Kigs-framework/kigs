23885617355973309544b7f28093acfa37f4dead
Stephane Capo <stephane.capo@next-bim.com>
2021-05-25 13:47:44 +0200

    git subrepo pull kigs
    subrepo:
      subdir:   "kigs"
      merged:   "dec9a7004a"
    upstream:
      origin:   "https://github.com/assoria/kigs.git"
      branch:   "master"
      commit:   "dec9a7004a"
    git-subrepo:
      version:  "0.4.1"
      origin:   "???"
      commit:   "???"
    
    Former-commit-id: 8d77d53d5913b9c3933387eac570a0569db26204
    Former-commit-id: 3d389eb97c855a07f0224d9b452a8bbc22e56c5d

--- libs/cross_platform/zlib/gzguts.h
+++ libs/cross_platform/zlib/gzguts.h
@@ -2,7 +2,10 @@
  * Copyright (C) 2004, 2005, 2010, 2011, 2012, 2013 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
+#ifndef WIN32
 #include <unistd.h>
+#endif
+
 
 #ifdef _LARGEFILE64_SOURCE
 #  ifndef _LARGEFILE_SOURCE
