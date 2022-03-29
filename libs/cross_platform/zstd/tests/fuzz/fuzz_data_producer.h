e82b8823045d95f1339854ef72760d80b57c1b90
Antoine Fresse <antoine.fresse@next-bim.com>
2022-03-29 14:31:05 +0200

    Updated to zstd-1.5.2

--- libs/cross_platform/zstd/tests/fuzz/fuzz_data_producer.h
+++ libs/cross_platform/zstd/tests/fuzz/fuzz_data_producer.h
@@ -1,10 +1,11 @@
 /*
- * Copyright (c) 2016-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc.
  * All rights reserved.
  *
  * This source code is licensed under both the BSD-style license (found in the
  * LICENSE file in the root directory of this source tree) and the GPLv2 (found
  * in the COPYING file in the root directory of this source tree).
+ * You may select, at your option, one of the above-listed licenses.
  */
 
 /**
@@ -47,6 +48,12 @@ int32_t FUZZ_dataProducer_int32Range(FUZZ_dataProducer_t *producer,
 
 /* Returns the size of the remaining bytes of data in the producer */
 size_t FUZZ_dataProducer_remainingBytes(FUZZ_dataProducer_t *producer);
+
+/* Rolls back the data producer state to have remainingBytes remaining */
+void FUZZ_dataProducer_rollBack(FUZZ_dataProducer_t *producer, size_t remainingBytes);
+
+/* Returns true if the data producer is out of bytes */
+int FUZZ_dataProducer_empty(FUZZ_dataProducer_t *producer);
 
 /* Restricts the producer to only the last newSize bytes of data.
 If newSize > current data size, nothing happens. Returns the number of bytes
