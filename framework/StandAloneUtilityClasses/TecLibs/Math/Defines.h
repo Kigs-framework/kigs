0e179f199c0697a30ff40b5c4032c7f53d7bb6e2
Stephane <stephane.capo@next-bim.com>
2023-01-05 11:29:49 +0100

    Replaced some defines with constexpr

--- framework/StandAloneUtilityClasses/TecLibs/Math/Defines.h
+++ framework/StandAloneUtilityClasses/TecLibs/Math/Defines.h
@@ -6,68 +6,47 @@
 // * COMMENT : ...
 // *---------------------------------------------------------------------
 // **********************************************************************
-#ifndef _Defines_h_
-#define _Defines_h_
+#pragma once
+
+namespace Kigs
+{
+	namespace Maths
+	{
+
+		// ----------------------------------------------------------------------
+		// +---------
+		// | Useful definitions
+		// +---------
+
+		constexpr float 	fPI = 		3.14159265358979f;
+		constexpr double 	PI = 		3.14159265358979f;
+		constexpr double 	PI_2 = 		(PI/2.0);
+		constexpr double 	PI_180 =	(PI/180.0);
+		constexpr float 	fPI_180 =	(fPI/180.0f);
 
 
+		// Other constants
 
-// ----------------------------------------------------------------------
-// +---------
-// | Useful definitions
-// +---------
-
-// PI constants (<math.h> notation: '_'=divide)
-#ifndef fPI
-#define fPI 3.14159265358979f
-#endif
-#ifndef PI
-#define PI          3.14159265358979
-#endif
-#ifndef PI_2
-#define PI_2        (PI/2.0)
-#endif
-#ifndef PI_180
-#define PI_180      0.01745329251994
-#endif
-#ifndef fPI_180
-#define fPI_180      0.01745329251994f
-#endif
-
-// Other constants
-#ifndef SQRT2
-#define SQRT2       1.41421356237309
-#endif
-#ifndef EPSILON
-#define EPSILON     0.000000000000001
-#endif
+		constexpr double 	SQRT2 =1.41421356237309;
+		constexpr double 	EPSILON =0.000000000000001;
 
 
-// Conversion operations
-#ifndef deg2rad
-#define deg2rad(Angle)      ((Angle)*PI_180)
-#endif
+		// Conversion operations
+		template<typename T>
+		constexpr T deg2rad(const T& angle) { return (T)(angle*PI_180); }
 
-// Sqr & cube operations
-/*#ifndef sqr
-#define sqr(x)              ((x)*(x))
-#endif*/
 
-template<typename T>
-constexpr T sqr(const T& x) { return x*x; }
+		template<typename T>
+		constexpr T sqr(const T& x) { return x*x; }
 
-/*
-#ifndef cube
-#define cube(x)             ((x)*(x)*(x))
-#endif
-*/
 
-template<typename T>
-constexpr T cube(const T& x) { return x*x*x; }
+		template<typename T>
+		constexpr T cube(const T& x) { return x*x*x; }
 
-#ifndef isnan
-#define isnan(x) ( !((x <= 0) || (x > 0)) )
-#endif
+		template<typename T>
+		constexpr bool isnan(const T& x) { return  ( !((x <= 0) || (x > 0)) ); }
 
-#endif//_Defines_h_
+	}
+}
 
 
