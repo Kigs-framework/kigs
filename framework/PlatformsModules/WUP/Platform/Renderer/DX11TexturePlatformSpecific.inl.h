897c93ce9d6363716c5d60819194706c8266624d
Kigs-framework <contact@kigs-framework.org>
2023-02-26 12:34:28 +0100

    UWP dynamic texture

--- framework/PlatformsModules/WUP/Platform/Renderer/DX11TexturePlatformSpecific.inl.h
+++ framework/PlatformsModules/WUP/Platform/Renderer/DX11TexturePlatformSpecific.inl.h
@@ -13,6 +13,16 @@ namespace Kigs
 
 		bool	DX11Texture::UpdateBufferZone(unsigned char* bitmapbuffer, const BBox2DI& zone, const Point2DI& bitmapSize)
 		{
+			RendererDX11* renderer = static_cast<RendererDX11*>(ModuleRenderer::mTheGlobalRenderer);
+			auto pd3dDevice = renderer->getDXInstance()->mDevice.get(); // Don't forget to initialize this
+
+			D3D11_MAPPED_SUBRESOURCE mappedResource;
+			auto pd3dContext = renderer->getDXInstance()->mDeviceContext; // Don't forget to initialize this
+			auto res = pd3dContext->Map(mTexturePointer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+
+			memcpy(mappedResource.pData, bitmapbuffer, mPow2Width * mPow2Height * 4);
+			pd3dContext->Unmap(mTexturePointer, 0);
+
 			return true;
 		}
 
